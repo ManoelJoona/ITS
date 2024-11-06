494b1b131b1d85da225bc8bb83ba6d07ddb978be
reiga <endo.reiga.24@shizuoka.ac.jp>
2024-11-07 04:07:00 +0900

    構造体の作成

--- CNN2.c
+++ CNN2.c
@@ -4,7 +4,35 @@
 
 #include <stdio.h>
 
-float node(void){
+typedef struct node{        /* 一つのnode */
     float value;
-}
+} Node;
 
+typedef struct edge{        /* 一本のedge */
+    Node* lhs;
+    Node* rhs;
+    float weight;
+} Edge;
+
+typedef struct nodevec{     /* nodeの縦方向のカタマリ */
+    Node* node;
+    Layer* next;
+} Layer;
+
+typedef struct Layer_vec{   /* "nodeの縦方向のカタマリ" の横方向のカタマリ */
+    Layer* layer;
+    Layer_vec* next;
+} Layer_vec;
+
+typedef struct edgevec{     /* "edgeの縦方向のカタマリ" の横方向のカタマリ */
+    Edge** edge;
+    Edge_layer* next;
+} Edge_layer;
+
+int edge_group[/* Node* lhs の数と Node* rhs の数の積 */] = {   /* edgeの縦方向のカタマリ */
+    
+};
+
+int main(void){
+    
+}
